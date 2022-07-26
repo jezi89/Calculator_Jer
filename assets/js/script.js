5a00aca95b76dccd94ce5b6c2aa473bd8c7c09e1
jezi89 <jer.vdzijde@gmail.com>
2022-07-26 15:06:41 +0200

    Error
    Comments Part 1
    
    Probleem
    
    Revert "Probleem"
    
    This reverts commit 2cbf58855978147bd6ddb672a7b4e9d573e8e58b.
    
    Revert "Comments Part 1"
    
    This reverts commit 42cfc23406d20428134f56075a1c23a22e83ab3a.
    
    Probleem

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,32 @@
+// Opdracht 1 LOI (10 punten)
+
+// Ik heb een retro stijl voor de calcculator gebruikt die ik nog wil upgraden naar een light/dark switchable theme. Ook wil ik er klik geluiden aan  toevoegen. In de generieke bootstrap icons wil ik de memory en statistieken bij gaan houden. De ruitjesachtergrond zou ik live overschrijfbaar maken voor notities.
+
+// Instructies:
+
+// 1. De gebruikte getallen slaat u op in let en const, geen var.
+
+// 2. U past alle bewerkingen toe (de operators +, -, * en /).
+
+// 3. U gebruikt conditionals om de juiste sommen te kunnen maken.
+
+// 4. U gebruikt de invoer zowel om op te tellen als samen te voegen tot string (concatenation).
+
+// 5. U schrijft eigen functies waar nodig. > Op meerdere plaatsen gedaan
+
+// 6. U zet waarden van variabelen om van data type (casting) waar nodig.
+
+// 7. U laat zien hoe u met globale en lokale variabelen werkt.
+
+// 8. U gebruikt Event handlers voor user events.
+
+// 9. Uw script werkt foutloos. > Edge cases geprobeerd uit te sluiten. Er is alleen een probleem als je op de min knop drukt na een berekening en vervolgens door wilt rekenen met het negatief gemaakte resultaat. Dit wil ik in een Patch.
+
+// 10. U hebt oplossingen gevonden voor alle uitdagingen. > Inlcusief extra functies Exponent en +/- knop
+
+// I want to build a Memory function in a later version, but already created the toggle menu to open the memory buttons.
+// Hereby I chose to keep the Memory button slightly brightened for as long as the memory menu is not hidden.
+// The css pseudo-class ::active was limited in that I could not have it prevent the Memory menu button from staying lid when it was clicked but when you dragged the mouse outside without opening the actual menu. Therefore I used the mousedown event for the pre-selection and listen to a mouseout to reset the styles. These are wrapped by a click event that should trigger if the click is made. This needs further testing on mobile and is likely not a responsive solution.
 const btnmem = document.getElementById('usemem');
 const toggleHide = document.getElementById('memrow');
 btnmem.style.filter = 'brightness(100%)';
@@ -5,7 +34,6 @@ btnmem.addEventListener('mousedown', function mouseDown(event) {
   this.style.filter = 'brightness(1.5)';
 });
 
-// This needs to be rewritten to target event
 btnmem.addEventListener('click', function onClick(event) {
   if (toggleHide.style.display === 'flex') {
     toggleHide.style.display = 'none';
@@ -21,6 +49,8 @@ btnmem.addEventListener('click', function onClick(event) {
   }
 });
 
+//  1. De gebruikte getallen slaat u op in let en const, geen var. > De button variabele zorgt voor
+// 7. U laat zien hoe u met globale en lokale variabelen werkt.
 let button = document.getElementsByName('button');
 
 // Setting data-attributes in Jer's JavaScript file allows
@@ -28,12 +58,15 @@ let button = document.getElementsByName('button');
 button.forEach((button) => {
   let btnVal = button.innerText;
 
-  // LONG OPTION
-  // if (btnVal == '*' || btnVal == '/' || btnVal == '-' || btnVal == '+') {
+  // 1st Long Option:
+  // if loop with conditional statement including (btnVal == '*' || btnVal == '/' || btnVal == '-' || btnVal == '+') {
   //   button.setAttribute('data-operation', '');
-  // 2nd MORE ADVANCED OPTION FOR NOW (faster)
+  // 2nd MORE ADVANCED OPTION(faster):
   // Use of a Set...
-  // 3rd OPTION
+  // 3rd chosen option:
+  // Like first option if else if loop with use of .includes and .isFinite OR .
+
+  // 3. U gebruikt conditionals om de juiste sommen te kunnen maken >  De data attribute operation wordt vervolgens gebruikt in de compute functie bij this.operation.
   if (['*', '/', '-', '+', 'EXP'].includes(btnVal)) {
     button.setAttribute('data-operation', '');
   } else if (btnVal == 'M++') {
@@ -72,7 +105,6 @@ class Calculator {
     // This clear function lets us set everything to default as as soon as we create a new calculator.
     // After load
     this.clear();
-    this.calcCheck();
   }
 
   // If everything needs to be cleared with AC, we need to reset all properties
@@ -84,10 +116,13 @@ class Calculator {
     this.operation = undefined;
   }
 
+  // The delete function
   delete() {
+    // 6. U zet waarden van variabelen om van data type (casting) waar nodig.
     this.currentOperand = this.currentOperand.toString().slice(0, -1);
   }
 
+  // I created a plus minus button that also checks for an empty string first ans sets this.plusminusNoDigit = true, to just show a minus symbol when pressed without a preceeding value. The subsequent pressed number is then converted to a negative number.
   plusminus() {
     if (this.currentOperand == '') {
       this.currentOperand = (this.currentOperand * -1).toString();
@@ -102,15 +137,17 @@ class Calculator {
     }
   }
 
+  // In order to be able to keep on typing a new calculation after result, I created this function that clears the current result when equals button is pressed (this.equalsCheck = true;) an previous operand is false (this.newCalc = true;). I got this to work to put calcCheck after the invokation of  calculator.compute(); &
+  // calculator.updateDisplay(). It seems to trigger the clear only once after the number button that is pressed after equals, but since newCalc and EqualsCheck and afterwards trigger the compute and updatedisplay function.
   calcCheck() {
     if (this.newCalc == true && this.equalsCheck == true) {
-      console.log('test');
       this.clear();
       this.newCalc = false;
       this.equalsCheck = false;
     }
   }
 
+  // Here we check if the pressed Numberbutton is a dot and if a dot is already in the CurrentOperand
   appendNumber(number) {
     if (number === '.' && this.currentOperand.includes('.')) return;
     if (this.currentOperandTextElement.innerText == '-') {
@@ -118,7 +155,9 @@ class Calculator {
       this.currentOperand = this.currentOperand.toString();
       return;
     }
+    // Cleaner solution would maybe be to use .toFixed(0)
     if (this.currentOperand.length <= 14) {
+      // 4. U gebruikt de invoer zowel om op te tellen als samen te voegen tot string (concatenation).
       this.currentOperand = this.currentOperand.toString() + number.toString();
     } else return;
   }
@@ -134,10 +173,13 @@ class Calculator {
     this.currentOperand = '';
   }
 
+  // 2. U past alle bewerkingen toe (de operators +, -, * en /).
   compute() {
     let computation;
+    // 7. U laat zien hoe u met globale en lokale variabelen werkt.
     const prev = parseFloat(this.previousOperand);
     const current = parseFloat(this.currentOperand);
+    // 3. U gebruikt conditionals om de juiste sommen te kunnen maken.
     if (isNaN(prev) || isNaN(current)) return;
     switch (this.operation) {
       case '+':
@@ -158,6 +200,7 @@ class Calculator {
       default:
         return;
     }
+    //Needs Revision and cleaner solution for correct big numbers (ie. https://github.com/MikeMcl/bignumber.js/)
     // if (computation >= 2 ** 53) {
     //   getDisplayNumber() = "Error";
     // }
@@ -168,7 +211,9 @@ class Calculator {
 
   // Helper function to make the numbers more definitive
   getDisplayNumber(number) {
+    // 1. De gebruikte getallen slaat u op in let en const, geen var.
     const stringNumber = number.toString();
+    // 6. U zet waarden van variabelen om van data type (casting) waar nodig.
     const integerDigits = parseFloat(stringNumber.split('.')[0]);
     const decimalDigits = stringNumber.split('.')[1];
     let integerDisplay;
@@ -229,6 +274,8 @@ const calculator = new Calculator(
   currentOperandTextElement
 );
 
+// 4. U gebruikt de invoer zowel om op te tellen als samen te voegen tot string (concatenation). > Indirect wordt de invoer gebruikt
+// 8. U gebruikt Event handlers voor user events.
 numberButtons.forEach((button) => {
   button.addEventListener('click', () => {
     calculator.calcCheck();
@@ -265,6 +312,8 @@ plusminusButton.addEventListener('click', (button) => {
   calculator.updateDisplay();
 });
 
+// Here we are setting the keyboard shortcuts for operationButtons.entriesAll are logical and for the +/- button I chose \
+// 8. U gebruikt Event handlers voor user events.
 document.addEventListener('keydown', function (event) {
   let patternForNumbers = /[0-9]/g;
   let patternForOperators = /[+\-*\/]/g;
